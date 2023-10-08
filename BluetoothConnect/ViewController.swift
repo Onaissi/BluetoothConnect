78fce8163dadae2e3c95afdfcb1f7e2d04645b1d
Onaissi <onaissiali@gmail.com>
2023-10-07 22:52:55 -0400

    Initial Commit

---
+++ BluetoothConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothConnect
+//
+//  Created by Ali Onaissi on 07/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
